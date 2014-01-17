3afeed26d52c6949d33e7749b48c8f06988271ff
Gary Bell <github@whiteneon.com>
2014-01-17 15:37:22 -0500

    Added Support for ATmega1284P Processor

--- Adafruit_CC3000.cpp
+++ Adafruit_CC3000.cpp
@@ -69,6 +69,10 @@ static const uint8_t dreqinttable[] = {
   20, 20, 21, 21, 22, 22, 23, 23, 24, 24,
   25, 25, 26, 26, 27, 27, 28, 28, 29, 29,
   30, 30, 31, 31, 32, 32, 33, 33,
+#elif  defined(__AVR_ATmega1284P__)
+	10, 0,
+	11, 1,
+	2, 3,
 #elif  defined(__AVR_ATmega32U4__) 
   7, 4,
   3, 0,
